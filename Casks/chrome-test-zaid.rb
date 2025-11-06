8edf6e68503df3ed2e05151ce26a363d61c13247
kurtadsheh99 <164123611+kurtadsheh99@users.noreply.github.com>
2025-11-06 12:12:03 +0300

    Add files via upload

---
+++ Casks/chrome-test-zaid.rb
@@ -0,0 +1,11 @@
+cask "chrome-test-zaid" do
+  version "142.0.7444.60"
+  sha256 "bf53302bca056c193e3dc9cfe6d921d168189b328662bd959310fea2d28df718"
+
+  url "https://dl.google.com/chrome/mac/universal/stable/CHFA/googlechrome.dmg"
+  name "Google Chrome"
+  desc "Web browser from Google"
+  homepage "https://www.google.com/chrome/"
+
+  app "Google Chrome.app"
+end
