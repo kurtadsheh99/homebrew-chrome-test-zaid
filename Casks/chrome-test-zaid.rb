4490e6b5636c612c3d98904c17d304ea1182a3bd
kurtadsheh99 <164123611+kurtadsheh99@users.noreply.github.com>
2025-11-06 12:24:18 +0300

    Update chrome-test-zaid.rb

--- Casks/chrome-test-zaid.rb
+++ Casks/chrome-test-zaid.rb
@@ -1,6 +1,6 @@
 cask "chrome-test-zaid" do
   version "142.0.7444.60"
-  sha256 "bf53302bca056c193e3dc9cfe6d921d168189b328662bd959310fea2d28df718"
+  sha256 "8bfc128d0bbd2341f5efcf6e23c7635aba49e4d42cefc62c10d9e1f173b8bd01"
 
   url "https://dl.google.com/chrome/mac/universal/stable/CHFA/googlechrome.dmg"
   name "Google Chrome"
